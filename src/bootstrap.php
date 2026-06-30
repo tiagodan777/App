afd623e46b8012826a048616cafc76667dba0662
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-06-30 14:37:17 +0000

    Update bootstrap.php

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -23,21 +23,21 @@ $twig_options['debug'] = DEV;
 $loader = new \Twig\Loader\FilesystemLoader([APP_ROOT . '/templates']);
 $twig = new Twig\Environment($loader, $twig_options);
 $twig->addGlobal('doc_root', DOC_ROOT);
-$twig->addExtension(new IntlExtension());
+/*$twig->addExtension(new IntlExtension());
 
 setlocale(LC_TIME, 'pt_PT.UTF-8', 'pt_PT', 'Portuguese_Portugal');
-date_default_timezone_set('Europe/Lisbon');
+date_default_timezone_set('Europe/Lisbon');*/
 
-$cookie = $cms->getCookie();
-$session = $cms->getSession();
+//$cookie = $cms->getCookie();
+//$session = $cms->getSession();
 $db = $cms->getDatabase();
 
 /*if ($cookie->token) {
     $session = $session->create($cookie->token);
 }*/
 
-$twig->addGlobal('cookie', $cookie);
-$twig->addGlobal('session', $session);
+//$twig->addGlobal('cookie', $cookie);
+//$twig->addGlobal('session', $session);
 
 if (DEV === true) {
     $twig->addExtension(new \Twig\Extension\DebugExtension);
