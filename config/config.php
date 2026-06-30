1c38a893aeb1159f6fe25986907c9222ce7ca46d
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-06-30 14:35:52 +0000

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,15 +4,15 @@ define("ROOT_FOLTER", 'public');
 define("DOC_ROOT", '/');
 define('DOMAIN', 'http://34.14.62.235/');
 
-/*$type = 'mysql';
+$type = 'mysql';
 $server = 'localhost';
-$db = 'lykrr';
+$db = 'app';
 $port = '3306';
 $charset = 'utf8mb4';
-$username = 'imperador_supremo_do_universo';
-$password = 'GUIDAMACAca687172euadorophp';
+$username = 'admin';
+$password = 'NovaFaseDaVidaEm2026';
 
-$dsn = "$type:host=$server;dbname=$db;port=$port;charset=$charset";*/
+$dsn = "$type:host=$server;dbname=$db;port=$port;charset=$charset";
 
 define('MEDIA_TYPES', ['image/jpeg', 'image/png', 'image/gif', 'image/webp', 'image/heic']);
 define('FILE_EXTENSIONS', ['jpeg', 'jpg', 'png', 'gif', 'webp', 'heic']);
