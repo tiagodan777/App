dc63dab8a16ca61cc0af5519093046a901d40d63
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-07-01 20:22:19 +0000

    ok

---
+++ public/js/websocket.js
@@ -0,0 +1,16 @@
+var ws = new WebSocket("ws://localhost:3306");
+
+var botao = window.document.querySelector('#botao');
+botao.addEventListener('click', enviar);
+
+function enviar() {
+    var largura = $(window).width();
+    var altura = $(window).height();
+
+    var data = {
+        largura: largura,
+        altura: altura
+    };
+
+    ws.send(JSON.stringify(data));
+}
