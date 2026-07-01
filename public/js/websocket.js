1802b63e6dfe065623f3a7c2fc762a570ade7020
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-07-01 20:28:11 +0000

    Update websocket.js

--- public/js/websocket.js
+++ public/js/websocket.js
@@ -1,4 +1,4 @@
-var ws = new WebSocket("ws://localhost:8080");
+var ws = new WebSocket('ws://' + location.host + '/ws');
 
 var botao = window.document.querySelector('#botao');
 botao.addEventListener('click', enviar);
