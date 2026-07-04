fa4e7253c117510423be2c248829b0a62fe87a24
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-07-04 15:06:11 +0000

    Update WebSocket.php

--- src/classes/CMS/WebSocket.php
+++ src/classes/CMS/WebSocket.php
@@ -12,7 +12,7 @@ use Ratchet\ConnectionInterface;
 class WebSocket implements MessageComponentInterface {
     protected $clients;
     private $pdo;
-    private $pessoas;
+    private $pessoas = [];
 
     public function __construct($pdoFactory) {
         $this->clients = new \SplObjectStorage;
@@ -21,60 +21,60 @@ class WebSocket implements MessageComponentInterface {
 
     public function onOpen(ConnectionInterface $conn) {
         $this->clients->attach($conn);
-        echo "Nova conexção!! - ({$conn->resourceId})\n";
-        
-        $nova_pessoa = [
-            'src' => '{{ doc_root }}imagens/fotos-perfil/tiago.webp',
+
+        echo "Nova conexão!! - ({$conn->resourceId})\n";
+
+        $this->pessoas[$conn->resourceId] = [
+            'id' => $conn->resourceId,
+            'src' => '/imagens/fotos-perfil/tiago.webp',
             'top' => random_int(50, 600),
             'left' => random_int(50, 400),
         ];
 
-        $this->pessoas[] = $nova_pessoa;
         var_dump($this->pessoas);
+
+        $this->broadcastNewState([
+            'type' => 'state',
+            'pessoas' => array_values($this->pessoas)
+        ]);
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+
+        unset($this->pessoas[$conn->resourceId]);
+
+        echo "Conexão ({$conn->resourceId}) desconectou-se\n";
+
+        $this->broadcastNewState([
+            'type' => 'state',
+            'pessoas' => array_values($this->pessoas)
+        ]);
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
         $data = json_decode($msg, true);
 
-        echo "ENVIAR\n";
-        var_dump($data);
-
-        if ($data[0]['type'] === 'move') {
-            $top = $data[0]['top'] ?? null;
-            $left = $data[0]['left'] ?? null;
-
-            echo "TOP: $top || LEFT: $left\n";
-
-            $data[1]['pessoas'][0]['top'] += $top;
-            $data[1]['pessoas'][0]['left'] += $left;
-
-            echo "NEW TOTAL TOP: ";
-            var_dump($data[1]['pessoas'][0]['top']);
-            echo "\nNEW TOTAL LEFT: ";
-            var_dump($data[1]['pessoas'][0]['left']);
+        if (($data['type'] ?? null) === 'move') {
+            $this->pessoas[$from->resourceId]['top'] += $data['top'] ?? 0;
+            $this->pessoas[$from->resourceId]['left'] += $data['left'] ?? 0;
         }
 
-        $this->broadcastNewState($data);
-    }
-
-    public function onClose(ConnectionInterface $conn) {
-        $this->clients->detach($conn);
-        echo "Conexção ({$conn->resouceId}) desconectou-se";
+        $this->broadcastNewState([
+            'type' => 'state',
+            'pessoas' => array_values($this->pessoas)
+        ]);
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e) {
-        echo "Ocorreu um erro: ({$e->getMessage()})\n";
+        echo "Erro: {$e->getMessage()}\n";
         $conn->close();
     }
 
     private function broadcastNewState($data) {
         foreach ($this->clients as $client) {
-            $this->sendNewState($client, $data);
+            $client->send(json_encode($data));
         }
-    }
-
-    private function sendNewState(ConnectionInterface $conn, $data) {
-        $conn->send(json_encode($data));
     }
 }
 
