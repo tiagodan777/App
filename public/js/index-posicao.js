2ebf2c98e8ae913d4bf29f0bd74922679e701000
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-07-04 19:22:24 +0000

    Update index-posicao.js

--- public/js/index-posicao.js
+++ public/js/index-posicao.js
@@ -1,4 +1,6 @@
 let dedos = {};
+let primeira_posicao = {}
+let c = 0;
 
 function atualizarDedos(e) {
     dedos = {};
@@ -8,12 +10,24 @@ function atualizarDedos(e) {
             x: touch.clientX,
             y: touch.clientY
         };
+        if (c == 0) {
+            primeira_posicao = {
+                x: touch.clientX,
+                y: touch.clientY
+            }
+        }
     }
 
+    console.log(primeira_posicao);
     console.log(dedos);
+}
+
+function reloadVarC() {
+    c = 0;
 }
 
 document.addEventListener("touchstart", atualizarDedos, { passive: false });
 document.addEventListener("touchmove", atualizarDedos, { passive: false });
 document.addEventListener("touchend", atualizarDedos, { passive: false });
+document.addEventListener("touchend", reloadVarC, { passive: false });
 document.addEventListener("touchcancel", atualizarDedos, { passive: false });
