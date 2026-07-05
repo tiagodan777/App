c024134f06397f9d5fc020edfbbf4f0099ea56a7
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-07-05 23:45:52 +0000

    Update index-tap-foto.js

--- public/js/index-tap-foto.js
+++ public/js/index-tap-foto.js
@@ -1,6 +1,6 @@
 $('.foto').on('click', function(e) {
     var top = $(this).css('top');
-    var left = $(this).css('left);
+    var left = $(this).css('left');
 
     console.log(top);
     console.log(left);
