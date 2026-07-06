047ecc5ff5aeae1acc8bb75bd0010db0db78fdc5
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-07-06 18:55:05 +0000

    Update create-account-navegacao.js

--- public/js/create-account-navegacao.js
+++ public/js/create-account-navegacao.js
@@ -1,3 +1,11 @@
 $(function() {
     $('form').load('/create-account-campos #nome');
+
+    $('nav.anterior-proximo > a').on('click', function(e) {
+        e.preventDefault();
+
+        var url = this.href;
+
+        $('form').load('/create-account-campos ' + url);
+    })
 });
