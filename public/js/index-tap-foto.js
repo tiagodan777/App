6fca19be4d302d5603665c2286f1a75dbed3cc4b
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-07-06 01:31:54 +0000

    Update index-tap-foto.js

--- public/js/index-tap-foto.js
+++ public/js/index-tap-foto.js
@@ -1,14 +1,14 @@
 $(function() {
     $(document).on('click', '.foto', function() {
-        var $backdrop = $('<div class="back-drop"></div>');
         var $div = $('<div class="mini-menu"></div>');
 
-        $backdrop.append($div);
         $('body').append($backdrop);
 
-        $backdrop.animate({
+        $div.animate({
             bottom: '0%'
         }, 500);
+
+        $div.focus();
     });
 
     $('.mini-menu').on('blur', function() {
