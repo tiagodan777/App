7e6325f41ac120baa2a05880fcb7d5625d6c1643
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-07-12 18:54:09 +0000

    Update Token.php

--- src/classes/CMS/Token.php
+++ src/classes/CMS/Token.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace App\CMS;
 
-class Token {
+class Token
+{
     private $db;
 
     public function __construct($db)
@@ -9,22 +11,73 @@ class Token {
         $this->db = $db;
     }
 
-    public function create($id, $proposito) {
-        $arguments['token'] = bin2hex(random_bytes(64));
-        $arguments['validade'] = date('Y-m-d H:i:s', strtotime('+20 mins'));
-        $arguments['membro_id'] = $id;
-        $arguments['proposito'] = $proposito;
+    public function create(
+        string $id,
+        string $proposito
+    ): string {
+        $arguments = [
+            'token' => bin2hex(random_bytes(64)),
+            'validade' => date(
+                'Y-m-d H:i:s',
+                strtotime('+20 minutes')
+            ),
+            'membro_id' => $id,
+            'proposito' => $proposito
+        ];
 
-        $sql = "INSERT INTO token (token, validade, membro_id, proposito)
-                VALUES (:token, :validade, :membro_id, :proposito);";
+        $sql = "
+            INSERT INTO token (
+                token,
+                validade,
+                membro_id,
+                proposito
+            )
+            VALUES (
+                :token,
+                :validade,
+                :membro_id,
+                :proposito
+            )
+        ";
+
         $this->db->runSQL($sql, $arguments);
+
         return $arguments['token'];
     }
 
-    public function getMemberId($token, $proposito) {
-        $arguments = ['token' => $token, 'purpose' => $proposito];
-        $sql = "SELECT membro_id FROM token
-                WHERE token = :token AND proposito = :proposito AND validade > NOW();";
-        return $this->db->runSQL($sql, $arguments)->fetchColumn();
+    public function getMemberId(
+        string $token,
+        string $proposito
+    ): string|false {
+        $arguments = [
+            'token' => $token,
+            'proposito' => $proposito
+        ];
+
+        $sql = "
+            SELECT membro_id
+            FROM token
+            WHERE token = :token
+            AND proposito = :proposito
+            AND validade > NOW()
+            LIMIT 1
+        ";
+
+        return $this->db
+            ->runSQL($sql, $arguments)
+            ->fetchColumn();
+    }
+
+    public function delete(
+        string $token
+    ): void {
+        $sql = "
+            DELETE FROM token
+            WHERE token = :token
+        ";
+
+        $this->db->runSQL($sql, [
+            'token' => $token
+        ]);
     }
 }
