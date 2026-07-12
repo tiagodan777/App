90102aad8999652fca45f77612dd0646d40895e4
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-07-12 19:01:43 +0000

    Update index.php

--- src/pages/index.php
+++ src/pages/index.php
@@ -6,11 +6,4 @@ require_login($session);
 
 $data['membro_id'] = $session->id;
 
-echo '<pre>';
-var_dump([
-    'session_id' => session_id(),
-    'session' => $_SESSION,
-]);
-echo '</pre>';
-
 echo $twig->render('index.html', $data);
