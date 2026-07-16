62f3fd35e0060a423c1f4250bd6b66a9937e36d7
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-07-16 14:14:53 +0000

    Update javascript-geral.js

--- public/js/javascript-geral.js
+++ public/js/javascript-geral.js
@@ -1,4 +1,6 @@
 $('#menuPrincipal > nav > ul > li > a').on('click', function(e) {
-    $('#menuPrincipal > nav > ul > li').removeClass('active');
+    $('#menuPrincipal > nav > ul > li > a').removeClass('active');
     $(this).addClass('active');
+
+    
 })
