9b0d3f403ddca4c83a4a0cce582797a0c0b7dd67
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-07-19 11:41:54 +0000

    ok

--- src/pages/profile.php
+++ src/pages/profile.php
@@ -9,6 +9,7 @@ $membro = $cms->getMember()->get($id);
 $idade = calcularIdade($membro['nascimento']);
 
 $data['membro'] = $membro;
+$data['primerio_gosto'] = $membro['gostos'][0];
 $data['idade'] = $idade;
 $data['id'] = $id;
 
