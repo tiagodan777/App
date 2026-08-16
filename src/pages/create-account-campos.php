1c6144a8cdb9bb8bc3054c43043e594c3c329e7b
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-08-16 11:58:00 +0000

    Update create-account-campos.php

--- src/pages/create-account-campos.php
+++ src/pages/create-account-campos.php
@@ -2,9 +2,18 @@
 
 declare(strict_types=1);
 
+use App\CMS\Member;
+use App\Validate\Validate;
+
 $modoEdicao = (($_GET['editar'] ?? '') === '1') && trim((string) ($session->id ?? '')) !== '';
 
 echo $twig->render('create-account-campos.html', [
     'modo_edicao' => $modoEdicao,
-    'ano_atual' => (int) date('Y')
+    'ano_atual' => (int) date('Y'),
+    'idade_minima' =>
+        Validate::MINIMUM_AGE,
+    'versao_termos' =>
+        Member::TERMS_VERSION,
+    'versao_privacidade' =>
+        Member::PRIVACY_VERSION
 ]);
