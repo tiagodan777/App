abd24b603d8f70bba03fe0c0434f7c95a26e8b36
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-08-17 16:25:29 +0000

    ok

--- android/app/src/main/java/com/margot/app/MainActivity.java
+++ android/app/src/main/java/com/margot/app/MainActivity.java
@@ -1,5 +1,14 @@
 package com.margot.app;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(BackgroundLocationPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
