7e652ec0e3f6939e1dc05160e9ce0ffd346426ae
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-08-28 09:41:38 +0000

    Update WebSocket.php

--- src/classes/CMS/WebSocket.php
+++ src/classes/CMS/WebSocket.php
@@ -13,7 +13,7 @@ use React\EventLoop\TimerInterface;
 
 class WebSocket implements MessageComponentInterface
 {
-    private const RAIO_MAXIMO_METROS = 40000;
+    private const RAIO_MAXIMO_METROS = 100;
     /* O iOS envia eventos de movimento/visita, não pings exatos por minuto. */
     private const LOCALIZACAO_MAXIMA_IDADE_SEGUNDOS = 180;
     private const LOCALIZACOES_PERSISTIDAS_CACHE_SEGUNDOS = 5;
