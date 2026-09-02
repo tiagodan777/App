8797f605ae77f1f5fe70265aa815dff3a642b86e
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-09-02 18:15:20 +0000

    ok

--- ios/App/App/ViewController.swift
+++ ios/App/App/ViewController.swift
@@ -11,7 +11,43 @@ class ViewController: CAPBridgeViewController {
         super.capacitorDidLoad()
 
         bridge?.registerPluginInstance(BackgroundLocationPlugin())
+
+        configurarScrollNativo()
         configurarGestosNavegacao()
+    }
+
+    private func configurarScrollNativo() {
+        guard let webView = webView else { return }
+
+        let scrollView = webView.scrollView
+
+        /*
+         Permite o rubber banding nativo do iOS.
+         */
+
+        scrollView.bounces = true
+
+        /*
+         Faz com que exista bounce vertical mesmo quando,
+         por alguma razão, a página não ultrapassa muito
+         a altura visível do ecrã.
+         */
+
+        scrollView.alwaysBounceVertical = true
+
+        /*
+         Não queremos rubber banding horizontal no WebView,
+         porque a Margot já tem os seus próprios swipes.
+         */
+
+        scrollView.alwaysBounceHorizontal = false
+
+        /*
+         Ajuda o iOS a distinguir movimentos principalmente
+         verticais dos horizontais.
+         */
+
+        scrollView.isDirectionalLockEnabled = true
     }
 
     private func configurarGestosNavegacao() {
@@ -31,7 +67,10 @@ class ViewController: CAPBridgeViewController {
             gesto.cancelsTouchesInView = true
 
             view.addGestureRecognizer(gesto)
-            webView.scrollView.panGestureRecognizer.require(toFail: gesto)
+
+            webView.scrollView.panGestureRecognizer.require(
+                toFail: gesto
+            )
 
             gestoVoltar = gesto
         }
@@ -48,17 +87,28 @@ class ViewController: CAPBridgeViewController {
             gesto.cancelsTouchesInView = true
 
             view.addGestureRecognizer(gesto)
-            webView.scrollView.panGestureRecognizer.require(toFail: gesto)
+
+            webView.scrollView.panGestureRecognizer.require(
+                toFail: gesto
+            )
 
             gestoAvancar = gesto
         }
     }
 
-    @objc private func tratarSwipe(_ gesto: UIScreenEdgePanGestureRecognizer) {
-        guard gesto.state == .ended, let webView = webView else { return }
+    @objc private func tratarSwipe(
+        _ gesto: UIScreenEdgePanGestureRecognizer
+    ) {
+        guard
+            gesto.state == .ended,
+            let webView = webView
+        else {
+            return
+        }
 
         let distancia = gesto.translation(in: view)
         let velocidade = gesto.velocity(in: view)
+
         let voltar = gesto.edges == .left
 
         let distanciaOK = voltar
@@ -69,21 +119,43 @@ class ViewController: CAPBridgeViewController {
             ? velocidade.x > 300
             : velocidade.x < -300
 
-        guard distanciaOK || velocidadeOK else { return }
+        guard distanciaOK || velocidadeOK else {
+            return
+        }
 
-        let comando = voltar ? "history.back();" : "history.forward();"
+        let comando = voltar
+            ? "history.back();"
+            : "history.forward();"
 
         let javascript = """
         (function () {
-            if (document.body.classList.contains('margot-mini-menu-aberto')) return false;
-            if (document.body.classList.contains('heys-abertos')) return false;
-            if (document.querySelector('dialog[open]')) return false;
+            if (
+                document.body.classList.contains(
+                    'margot-mini-menu-aberto'
+                )
+            ) return false;
+
+            if (
+                document.body.classList.contains(
+                    'heys-abertos'
+                )
+            ) return false;
+
+            if (
+                document.querySelector(
+                    'dialog[open]'
+                )
+            ) return false;
 
             \(comando)
+
             return true;
         })();
         """
 
-        webView.evaluateJavaScript(javascript, completionHandler: nil)
+        webView.evaluateJavaScript(
+            javascript,
+            completionHandler: nil
+        )
     }
 }
