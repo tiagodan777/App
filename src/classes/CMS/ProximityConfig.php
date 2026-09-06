1f536ae1c0af07f13cfd91031c545b6a8a624986
Tiago Daniel <tiagoamdaniel488@gmail.com>
2026-09-06 21:08:57 +0000

    ok

--- src/classes/CMS/ProximityConfig.php
+++ src/classes/CMS/ProximityConfig.php
@@ -16,7 +16,7 @@ final class ProximityConfig
      * 100.0 = 100 metros
      * 150.0 = 150 metros
      */
-    public const RADIUS_METRES = 40000;
+    public const RADIUS_METRES = 1600000;
 
     /**
      * Calcula automaticamente o bounding box necessário
